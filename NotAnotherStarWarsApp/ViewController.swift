02d0ee24e9a1c578fe1d895946e07b857a3fa0c9
Jose Antonio García Yañez <joseantoniogarciay@gmail.com>
2017-02-01 18:16:59 +0100

    Initial Commit

---
+++ NotAnotherStarWarsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotAnotherStarWarsApp
+//
+//  Created by Jose Antonio García Yañez on 1/2/17.
+//  Copyright © 2017 RocketSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
